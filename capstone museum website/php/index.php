3f0468a903f2bbd4697a65a6abe9eca56c4b2d8e
chase <ch83311s@yahoo.com>
2018-04-05 16:50:48 -0400

    Add files via upload
    Updated files. To include .js , .php, .css, .html

---
+++ capstone museum website/php/index.php
@@ -0,0 +1,70 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+		<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" />
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link rel="stylesheet" type="text/css" href="css/scrolling-nav.css" />
+		<title>Lion Group | 2010 - present</title>
+	</head>
+	<body id="page-top">
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+			<div class="container">
+				<a class="navbar-brand js-scroll-trigger" href="index.php">Lion Group</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarResponsive">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<a class="nav-link js-scroll-trigger" href="1960s.php">1960s</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link js-scroll-trigger" href="1970s.php">1970s</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link js-scroll-trigger" href="1980s.php">1980s</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link js-scroll-trigger" href="1990s.php">1990s</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link js-scroll-trigger" href="2000s.php">2000s</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link js-scroll-trigger" href="2010s.php">Present</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>		<header class="bg-secondary text-white">
+			<div class="container text-center">
+				<h1>2010 - present</h1>
+				<p class="lead">subtext</p>
+			</div>
+		</header>
+		<section id="cont">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 mx-auto">
+						<h2>-------</h2>
+						<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rhoncus, purus sed pharetra congue, sapien arcu venenatis purus, eget imperdiet ligula augue in odio. Quisque faucibus massa consequat, convallis nunc id, commodo quam. Vestibulum vel felis molestie, bibendum mi id, luctus erat. Nam accumsan aliquet ipsum et imperdiet. Aenean lacus ligula, scelerisque ut ligula a, mollis dapibus libero. Aliquam pharetra tristique nunc non laoreet. Donec et justo commodo, lobortis mauris id, pretium mi. Praesent et odio euismod, volutpat tellus vel, eleifend libero. Quisque vitae sagittis sapien. Proin aliquam feugiat placerat. In dapibus, ligula ac tempus tempor, dolor sem tincidunt elit, nec vestibulum nibh orci eu eros. Donec pulvinar augue nec felis imperdiet luctus.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer class="py-5 bg-dark">
+			<div class="container">
+				<p class="m-0 text-center text-white">Copyright &copy; Lion Group 2018</p>
+			</div>
+		</footer>
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="js/scrolling-nav.js"></script>
+	</body>
+</html>
